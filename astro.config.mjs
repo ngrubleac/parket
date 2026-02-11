b394fe8f0fe1f6d8ad80ef3183479ae7be43f73b
Nicolae Grubleac <n.grubleac@gmail.com>
2026-02-11 16:55:44 +0200

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,17 @@ import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://parchetmaster.vercel.app', // Замените на ваш реальный домен
   adapter: vercel(),
   integrations: [
     tailwind(),
     react(),
-    keystatic()
+    keystatic(),
+    sitemap()
   ],
   output: 'static',
   i18n: {
